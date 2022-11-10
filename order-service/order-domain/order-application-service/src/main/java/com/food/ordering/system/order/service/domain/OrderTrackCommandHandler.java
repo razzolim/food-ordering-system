9cfbba62b108af830697916d22d0f7fa91a70709
Renan Azzolim <azzolimrenan2@gmail.com>
2022-11-10 20:52:34 -0300

    Implementing Order Track Command Handler

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -2,14 +2,37 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.track.TrackerOrderQuery;
 import com.food.ordering.system.order.service.domain.dto.track.TrackerOrderResponse;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class OrderTrackCommandHandler {
 
+    private final OrderDataMapper orderDataMapper;
+    private final OrderRepository orderRepository;
+
+    public OrderTrackCommandHandler(OrderDataMapper orderDataMapper, OrderRepository orderRepository) {
+        this.orderDataMapper = orderDataMapper;
+        this.orderRepository = orderRepository;
+    }
+
+    @Transactional(readOnly = true)
     public TrackerOrderResponse trackOrder(TrackerOrderQuery trackerOrderQuery) {
-        return null;
+        Optional<Order> orderResult = orderRepository.findByTrackingId(new TrackingId(trackerOrderQuery.getOrderTrackingId()));
+
+        if (orderResult.isEmpty()) {
+            log.warn("Could not find order with tracking id: {}", trackerOrderQuery.getOrderTrackingId());
+            throw new OrderNotFoundException("Could not find order with tracking id: " + trackerOrderQuery.getOrderTrackingId());
+        }
+        return orderDataMapper.orderToTrackOrderResponse(orderResult.get());
     }
 }
---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Valid
+@Service
+public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+
+    @Override
+    public void paymentCompleted(PaymentResponse paymentResponse) {
+
+    }
+
+    @Override
+    public void paymentCancelled(PaymentResponse paymentResponse) {
+
+    }
+}
